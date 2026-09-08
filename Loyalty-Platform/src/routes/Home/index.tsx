a9b37d1779259bcece4f5141a23a32b400822dea
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-08 20:35:10 -0300

    feat: adiciona estrutura html na página home

--- Loyalty-Platform/src/routes/Home/index.tsx
+++ Loyalty-Platform/src/routes/Home/index.tsx
@@ -1,8 +1,57 @@
 // src/routes/Home/index.tsx
+import logoPrincipal from "../../img/LOGO PRINCIPAL.png";
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <header className="conteudo-principal">
+        <img src={logoPrincipal} alt="logo principal" />
+        <div>
+          <h1>Gamificação que engaja.</h1>
+          <h2 id="degrade">Resultados que fidelizam.</h2>
+        </div>
+      </header>
+
+      <section className="sobre-nos">
+        <h1>Quem somos ?</h1>
+
+        <p>
+          A <span id="degrade">Loyalty Platform</span> transforma o
+          engajamento dos seus usuários em recompensas reais, por meio de
+          personalizada, sem precisar construir essa estrutura do zero!
+        </p>
+      </section>
+
+      <section className="card-section">
+        <div className="feature-card">
+          <h2 id="titulo-card">Missão</h2>
+          <p>
+            Oferecer gamificação personalizada que conecta o comportamento
+            digital dos usuários a recompensas reais.
+          </p>
+        </div>
+
+        <div className="feature-card">
+          <h2 id="titulo-card">Visão</h2>
+          <p>Resolver os problemas e alcançar o FIAP NEXT</p>
+        </div>
+
+        <div className="feature-card">
+          <h2 id="titulo-card">Valores</h2>
+          <ul>
+            <li>Inovação</li>
+            <li>Personalização</li>
+            <li>Resultado real</li>
+            <li>Escalabilidade</li>
+            <li>Impacto positivo</li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="botao-wrapper">
+        <a className="botao" href="/sobre">
+          Conheça a solução!
+        </a>
+      </div>
     </main>
   );
 }
