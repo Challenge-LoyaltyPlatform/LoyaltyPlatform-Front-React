52fb375b3276393f5e5ce855e244e6a62d7cd7f0
Luiz Felipe <sluizfelipe692@gmail.com>
2026-09-11 01:14:22 -0300

    styles: deixa logo da loyalty responsiva

--- Loyalty-Platform/src/routes/Contato/index.tsx
+++ Loyalty-Platform/src/routes/Contato/index.tsx
@@ -3,6 +3,16 @@ export default function Home() {
   return (
     <main>
       <h1>Contato</h1>
+
+       {/* BOTÃO */}
+
+        <div className="mt-10 flex justify-center">
+        <a href="/sobre"
+          className="inline-block bg-[#6C4DFC] hover:bg-[#5b3ce6] transition text-white text-base font-bold px-10 py-4 rounded-full"
+  >
+            Enviar mensagem!
+        </a>
+      </div>
     </main>
   );
 }
